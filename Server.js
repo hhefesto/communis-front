87bf3bccc6a4bcaa136265a4495312c86fe2da89
hefesto <daniel.herrera.rendon@gmail.com>
2016-01-25 23:36:23 -0600

    initial commit

---
+++ Server.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname + '/views/';
+
+router.use(function (req,res,next) {
+    console.log("/" + req.method);
+    next();
+});
+
+router.get("/",function(req,res){
+    res.sendFile(path + "index.html");
+});
+
+router.get("/about",function(req,res){
+    res.sendFile(path + "about.html");
+});
+
+router.get("/contact",function(req,res){
+    res.sendFile(path + "contact.html");
+});
+
+router.get("/bootstrap.min.css",function(req,res){
+    res.sendFile(path + "bootstrap.min.css");
+});
+
+app.use("/",router);
+
+app.use("*",function(req,res){
+    res.sendFile(path + "404.html");
+});
+
+app.listen(3000,function(){
+    console.log("Live at Port 3000");
+});
