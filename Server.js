cf7fad76dac2b07e6e2f2fed49c2445671206f80
hefesto <daniel.herrera.rendon@gmail.com>
2016-03-21 15:49:31 -0600

    posts y post a medias.

--- Server.js
+++ Server.js
@@ -8,9 +8,19 @@ router.use(function (req,res,next) {
     next();
 });
 
+app.use(express.static('public'));
+
 router.get("/",function(req,res){
     console.log("request address: " + req.connection.remoteAddress)
     res.sendFile(path + "index.html");
+});
+
+router.get("/posts",function(req,res){
+    res.sendFile(path + "posts.html");
+});
+
+router.get("/post",function(req,res){
+    res.sendFile(path + "post.html");
 });
 
 router.get("/levels",function(req,res){
@@ -25,9 +35,9 @@ router.get("/contact",function(req,res){
     res.sendFile(path + "contact.html");
 });
 
-router.get("/bootstrap.min.css",function(req,res){
-    res.sendFile(path + "bootstrap.min.css");
-});
+// router.get("/bootstrap.min.css",function(req,res){
+//     res.sendFile(path + "bootstrap.min.css");
+// });
 
 app.use("/",router);
 
